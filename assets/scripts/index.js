85e396a7afcf3d56f56cce6a0bd6b0d9930b6828
dvdpwll <dvdpwll456@gmail.com>
2016-09-05 11:09:49 -0400

    refactored index.js
    removed folder handlers from index.js

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,10 +1,8 @@
 'use strict';
 
 const authEvents = require('./auth/events.js');
-const folderEvents = require('./folder/events.js');
 
 // On document ready
 $(() => {
   authEvents.addHandlers();
-  folderEvents.addHandlers();
 });
