4e088ff6deb43a9642df42b96a974951eac3b8de
gabescarbrough <gabescarbrough@gmail.com>
2016-09-06 13:52:31 -0400

    Add sidebar
    Will use to show users with links to their folders

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -4,5 +4,8 @@ const authEvents = require('./events.js');
 
 // On document ready
 $(() => {
-  authEvents.addHandlers();
+  $("#menu-toggle").click(function(e) {
+      e.preventDefault();
+      $("#wrapper").toggleClass("toggled");
+  });
 });
