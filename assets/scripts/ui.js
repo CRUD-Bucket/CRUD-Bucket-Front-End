cab60706130add75b9c821340ea2f386dafdfb55
gabescarbrough <gabescarbrough@gmail.com>
2016-09-06 19:29:52 -0400

    Write code to view other users' root folders
    Clicking username in sidebar shows contents of their root folder

--- assets/scripts/ui.js
+++ assets/scripts/ui.js
@@ -20,36 +20,10 @@ const createSuccess = () => {
   console.log('folder created');
 };
 
-const displayUserFolder = function(data){
-  console.log(data);
-  let userFolderTemplate = require('./templates/current-user-folder.handlebars');
-    $('#main-content').html(userFolderTemplate({
-      folders: data.folders
-    }));
-};
-
-const displayUserFile = function(data){
-  console.log(data);
-  let userFileTemplate = require('./templates/current-user-file.handlebars');
-    $('#main-content').append(userFileTemplate({
-      files: data.files
-    }));
-};
-
-const displayUsers = function(data){
-  let userTemplate = require('./templates/user.handlebars');
-  $('.sidebar-nav').html(userTemplate({
-      users: data.users,
-    }));
-};
-
 module.exports = {
   failure,
   success,
   signOutSuccess,
   app,
   createSuccess,
-  displayUsers,
-  displayUserFolder,
-  displayUserFile,
 };
